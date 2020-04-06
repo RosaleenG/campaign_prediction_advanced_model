c87673763a116fd6c33489c73f5253be9eb1936a
RosaleenG <rosie_goldrick@hotmail.com>
2020-04-06 09:57:03 +0100

    added a campaign prediction model

---
+++ model.py
@@ -0,0 +1,13 @@
+def build_model():
+    """This function builds a new model and returns it.
+
+    The model should be implemented as a sklearn Pipeline object.
+
+    Your pipeline needs to have two steps:
+    - preprocessor: a Transformer object that can transform a dataset
+    - model: a predictive model object that can be trained and generate predictions
+
+    :return: a new instance of your model
+    """
+
+    raise NotImplementedError
